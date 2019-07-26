0421c9630f6f5a7a9e58b38af84cb4ecbb3eecbd
xtsky <1143754412@qq.com>
2019-07-26 22:36:07 +0800

    too

--- src/wugong/HelloWorld.java
+++ src/wugong/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.err.println("hello I'm is xtsky , and you ?");
 		System.out.println("My name is YanLiu , naisi to meet you!");
+		System.out.println("naisi to meet you too!");
 	}
 	
 }
